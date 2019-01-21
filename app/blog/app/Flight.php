7a1e97ce31f0db51c7e420948be62207df91501e
logickee <logickee@gmail.com>
2019-01-22 01:56:46 +0800

    update,

---
+++ app/blog/app/Flight.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Flight extends Model
+{
+    //
+    protected $table = 'my_fight';
+    public $timestamps = false;
+    protected $dateFormat = 'U';
+
+
+
+}
