4546fa44428b58f75d1345d9660619d0524be97d
logickee <logickee@gmail.com>
2019-01-22 17:13:06 +0800

    update for docker build files,

--- fabfile.py
+++ fabfile.py
@@ -10,6 +10,7 @@ from fabric.context_managers import *
 from fabric.contrib.project import *
 
 CWD = os.path.dirname(__file__)
+DOCKER_DIR = os.path.join(CWD,'_docker')
 
 def docker_compose_restart():
     with lcd(CWD):
@@ -17,3 +18,13 @@ def docker_compose_restart():
         # local('docker cp ./_settings/apache2.conf laravel-tryout:/etc/apache2/sites-available/000-default.conf')
         local('docker-compose restart')
         print('restart done')
+
+
+def reprovision_docker():
+    with lcd(DOCKER_DIR):
+            local('docker-compose kill')
+            local('docker-compose down')
+
+            local('docker-compose build')
+
+            local('docker-compose up')
