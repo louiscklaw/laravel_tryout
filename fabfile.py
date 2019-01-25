720c20f1c54452e2facf58bd1dfc999fa30d2c2d
logickee <logickee@gmail.com>
2019-01-25 22:54:54 +0800

    update build step,

--- fabfile.py
+++ fabfile.py
@@ -12,6 +12,21 @@ from fabric.contrib.project import *
 CWD = os.path.dirname(__file__)
 DOCKER_DIR = os.path.join(CWD,'_docker')
 
+
+
+def docker_compose_cmd(cmd_body):
+    local('docker-compose {}'.format(cmd_body) )
+
+def docker_compose_exec(cmd_body):
+    docker_compose_cmd(' exec {}'.format(cmd_body) )
+
+def docker_compose_php_composer_install():
+    docker_compose_exec("web /bin/sh -c 'cd /app/blog; composer install'")
+
+
+def laravel_artisan_migrate():
+    docker_compose_exec("web /bin/sh -c 'cd /app/blog; php artisan migrate'")
+
 def docker_compose_restart():
     with lcd(DOCKER_DIR):
         local('docker cp ./_settings/000-default.conf laravel-php7:/etc/apache2/sites-available/000-default.conf')
@@ -19,6 +34,10 @@ def docker_compose_restart():
         local('docker-compose restart')
         print('restart done')
 
+def laravel_rebuild():
+    with lcd(DOCKER_DIR):
+        docker_compose_php_composer_install()
+        laravel_artisan_migrate()
 
 def rebuild_docker():
     with lcd(DOCKER_DIR):
