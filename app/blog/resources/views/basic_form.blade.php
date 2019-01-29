fb8a91d3b916a1466356e58df2d555878c0103f4
logickee <logickee@gmail.com>
2019-01-30 04:13:22 +0800

    clear date picket,

--- app/blog/resources/views/basic_form.blade.php
+++ app/blog/resources/views/basic_form.blade.php
@@ -255,36 +255,6 @@
             </div>
         </div>
 
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="header">
-                        <h2>
-                            BOOTSTRAP DATE PICKER
-                            <small>Taken from <a href="https://github.com/uxsolutions/bootstrap-datepicker" target="_blank">github.com/uxsolutions/bootstrap-datepicker</a></small>
-                        </h2>
-                        <ul class="header-dropdown m-r--5">
-                            <li class="dropdown">
-                                <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                    aria-haspopup="true" aria-expanded="false">
-                                    <i class="material-icons">more_vert</i>
-                                </a>
-                                <ul class="dropdown-menu pull-right">
-                                    <li><a href="javascript:void(0);">Action</a></li>
-                                    <li><a href="javascript:void(0);">Another action</a></li>
-                                    <li><a href="javascript:void(0);">Something else here</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="body">
-                        <div class="row clearfix">
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
         <!--#END# Bootstrap Date Picker -->
         <!--DateTime Picker -->
         <div class="row clearfix">
